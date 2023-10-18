d5b70b8e27ed310e947d1e556c18129322779701
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-10-18 19:21:01 +0000

    refactor: beautify

--- algorithms/combination.cpp
+++ algorithms/combination.cpp
@@ -4,7 +4,7 @@
 
 /**
  * ## Combination : nCr
- * 
+ *
  * ### Initialization:
  * ```cpp
  * Combination combination(n, r);
@@ -15,29 +15,35 @@
  *  combination.next();
  *  combination.current_state();
  * ```
-*/
-class Combination {
+ */
+class Combination
+{
 private:
-	std::vector<bool> selection_state;
-	int n;
+    std::vector<bool> selection_state;
+    int n;
+
 public:
-	Combination(int _n, int _r) {
-		n = _n;
-		selection_state = std::vector<bool>(n, false);
-		for(int i = 0; i < _r; ++i)
-			selection_state[i] = true;
-	}
+    Combination(int _n, int _r)
+    {
+        n = _n;
+        selection_state = std::vector<bool>(n, false);
+        for (int i = 0; i < _r; ++i)
+            selection_state[i] = true;
+    }
 
-	bool next() {
-		return next_permutation(rbegin(selection_state), rend(selection_state));
-	}
+    bool next()
+    {
+        return next_permutation(rbegin(selection_state), rend(selection_state));
+    }
 
-	bool is_selected(int i) {
-		assert(i < n);
-		return selection_state[i];
-	}
+    bool is_selected(int i)
+    {
+        assert(i < n);
+        return selection_state[i];
+    }
 
-	std::vector<bool> current_state() {
-		return selection_state;
-	}
+    std::vector<bool> current_state()
+    {
+        return selection_state;
+    }
 };
