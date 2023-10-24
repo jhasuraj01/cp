76be7bcfc5719651268ec0593f7077e635433595
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-10-24 15:47:35 +0000

    feat: updated template

--- notes/sliding_window.cpp
+++ notes/sliding_window.cpp
@@ -1,23 +1,35 @@
-template <typename T>
-T sliding_window(vector<T> &nums) {
+#include<vector>
+using std::vector;
+
+/**
+ * ## Sliding Window
+ *
+ * @ref https://leetcode.com/problems/frequency-of-the-most-frequent-element/solutions/1175088/C++-Maximum-Sliding-Window-Cheatsheet-Template/
+ */
+
+int sliding_window(vector<int> &nums) {
   int n = nums.size();
   int start = 0;
   int end = 0;
 
-  T curr_value;
-  T final_value;
-  
-  while(start < n) {
-      if(end < n && /* condition for inclusion */) {
-          // add element at `end` to curr_value
-          end++;
-      }
-      else {
-          // remove element at `start` from curr_value
-          start++;
-      }
+  int curr_value;
+  int final_value;
+
+  while (end < n) {
+
+    // add element at `end` to curr_value
+
+    while (start < end  /* && condition for exclusion */) {
       int curr_len = end - start;
-      // update `final_value` using `curr_value`
+
+      // remove element at `start` from curr_value
+
+      start++;
+    }
+
+    // update `final_value` using `curr_value`
+
+    end++; // position of this line may vary from problem to problem
   }
 
   return final_value;
