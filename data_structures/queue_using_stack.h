e62f9ed190b71999698b2740468573f844e320d5
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2023-10-25 16:57:32 +0000

    feat: implemented `stack` using `queue`

---
+++ data_structures/queue_using_stack.h
@@ -0,0 +1,35 @@
+
+template <typename T>
+class QueueUsingStack {
+   private:
+    stack<T> s1, s2;
+    void preprocess() {
+        if (!s2.empty()) return;
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+    }
+
+   public:
+    QueueUsingStack() {
+    }
+
+    void push(T x) {
+        s1.push(x);
+    }
+
+    void pop() {
+        preprocess();
+        s2.pop();
+    }
+
+    int top() {
+        preprocess();
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.size() + s2.size() == 0;
+    }
+};
